64629ce33b93a4de394722bb41043e833ff7a529
worka <adiaz616@fiu.edu>
2025-02-19 21:26:33 -0500

    Add project files.

---
+++ TaskManagerApp/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagerApp.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
